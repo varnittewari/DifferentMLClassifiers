db0433aefa94ef060082807cdf027c616173aef6
varnittewari <vxt6823@rit.edu>
2019-12-18 21:50:00 -0500

    fixed the classifiers error

--- MLClassifiers.py
+++ MLClassifiers.py
@@ -15,6 +15,7 @@ y= data['Species']
 #Split the Data into Training and Testing sets with test size as #30%
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, shuffle=True)
 
+classifiers = []
 model1 = xgboost.XGBClassifier()
 classifiers.append(model1)
 model2 = svm.SVC()
@@ -24,3 +25,10 @@ classifiers.append(model3)
 model4 = RandomForestClassifier()
 classifiers.append(model4)
 
+for clf in classifiers:
+    clf.fit(X_train, y_train)
+    y_pred= clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print("Accuracy of %s is %s"%(clf, acc))
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix of %s is %s"%(clf, cm))
