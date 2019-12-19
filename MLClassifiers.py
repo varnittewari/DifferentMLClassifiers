15c3c41a2cd44d9db82fc3be4cc91bd6d1189ca5
varnittewari <vxt6823@rit.edu>
2019-12-18 21:19:15 -0500

    split the data into training and testing

--- MLClassifiers.py
+++ MLClassifiers.py
@@ -6,10 +6,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm, tree
 import xgboost
 from sklearn.model_selection import train_test_split
+
 data  =  pd.read_csv("D://Blogs//Iris Multiple Algo//Iris.csv")
 #Create Dependent and Independent Datasets based on our Dependent #and Independent features
 X  = data[['SepalLengthCm','SepalWidthCm','PetalLengthCm']]
 y= data['Species']
+
 #Split the Data into Training and Testing sets with test size as #30%
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, shuffle=True)
+
+
 
